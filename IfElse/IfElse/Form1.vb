c0fee87b77e169e4f372c161b141114590fd7dc0
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:15:24 -0500

    work day

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,20 +11,32 @@ Public Class Form1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
-        num1 = CDbl(st1)
-        num2 = CDbl(st2)
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type the numbers!")
+            Return
+        End If
+
+            num1 = CDbl(st1)
+            num2 = CDbl(st2)
+
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type non-negative numbers!")
+            Return
+        End If
 
         Dim result As String
 
-        If num1 > num2 Then
-            result = "The larger number is " & num1
-        ElseIf num2 > num1 Then
-            result = "The larger number is " & num2
-        Else
-            result = "They are equal"
-        End If
+            If num1 > num2 Then
+                result = "The larger number is " & num1
+            ElseIf num2 > num1 Then
+                result = "The larger number is " & num2
+            Else
+                result = "They are equal"
+            End If
 
-        txtResult.Text = result
+            txtResult.Text = result
+
+
     End Sub
 
 End Class
